277fa08f0d4fef7dae1064242d52a699f5bfb1d9
David Adams <david.riding.rails@gmail.com>
2018-12-15 12:22:03 -0700

    Add styling to devise pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :recoverable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
   has_many :stories
   has_many :notes
