33be1cdb64736959937074c5feaaf43f66665a9d
David Adams <david.riding.rails@gmail.com>
2019-01-31 19:43:14 -0700

    Updating story validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,7 @@
 class Story < ApplicationRecord
 
+  validates :title, presence: true, length: { in: 1..50 }
+  validates :user_id, numericality: true
 	has_many :notes, dependent: :destroy
 	belongs_to :user
 	
