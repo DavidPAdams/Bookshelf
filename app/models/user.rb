8ba3a7898b4dbb3f3e9b51a67fd13be2a68fc84f
David Adams <david.riding.rails@gmail.com>
2019-01-31 19:43:32 -0700

    Updating user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+
   # Include default devise modules. Others available are:
   # :confirmable, :recoverable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, 
          :validatable
 
-  validates :email, presence: true, uniqueness: true
-  # validates :username, presence: true, uniqueness: true 
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  # validates :username, presence: true, length: { in: 3..20 },
+  #                     uniqueness: { case_sensitive: false }
 
   has_many :stories, dependent: :destroy
   has_many :notes, dependent: :destroy
